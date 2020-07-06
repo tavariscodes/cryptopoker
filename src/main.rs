159b93776e6c635089f33b7a81655334cf11fba0
tavariscodes <tavariscodes@gmail.com>
2020-07-05 23:06:34 -0500

    deck

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use pokersite::deck::*;
+
 fn main() {
-    println!("Hello, world!");
+    let test_deck = Deck::new();
+    println!("There are {:?} cards in the deck", test_deck.cards.len());
+
 }
